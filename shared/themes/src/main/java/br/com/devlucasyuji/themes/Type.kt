5deaeb7aa99c7039cb0c25cd2fa09b56e3b309dc
devLucasYuji <lucasyujideveloper@gmail.com>
2022-03-31 00:12:44 -0300

    feat: add poppins font family to typography

--- shared/themes/src/main/java/br/com/devlucasyuji/themes/Type.kt
+++ shared/themes/src/main/java/br/com/devlucasyuji/themes/Type.kt
@@ -2,17 +2,17 @@ package br.com.devlucasyuji.themes
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import br.com.devlucasyuji.camerareminder.R
 
-// Set of Material typography styles to start with
 val Typography = Typography(
-        bodyLarge = TextStyle(
-                fontFamily = FontFamily.Default,
-                fontWeight = FontWeight.Normal,
-                fontSize = 16.sp,
-                lineHeight = 24.sp,
-                letterSpacing = 0.5.sp
-        )
+    bodyLarge = TextStyle(
+        fontFamily = FontFamily(
+            Font(R.font.poppins_bold, FontWeight.Bold),
+            Font(R.font.poppins_regular, FontWeight.Normal)
+        ),
+    )
 )
