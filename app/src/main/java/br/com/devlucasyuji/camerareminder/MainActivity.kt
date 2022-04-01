decfb26826d21f3514ac4b39713813bda6bf3d19
devLucasYuji <lucasyujideveloper@gmail.com>
2022-04-01 01:15:24 -0300

    feat: add splash screen and improve icon launcher

--- app/src/main/java/br/com/devlucasyuji/camerareminder/MainActivity.kt
+++ app/src/main/java/br/com/devlucasyuji/camerareminder/MainActivity.kt
@@ -3,13 +3,16 @@ package br.com.devlucasyuji.camerareminder
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import br.com.devlucasyuji.themes.CameraReminderTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         setContent {
             CameraReminderTheme {
+
             }
         }
     }
