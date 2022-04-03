2975395002b80221f5389331219e93722f16ba66
devLucasYuji <lucasyujideveloper@gmail.com>
2022-04-02 21:22:12 -0300

    feat: add hilt on configuration gradle to all modules use

---
+++ app/src/main/java/br/com/devlucasyuji/camerareminder/CameraReminderApplication.kt
@@ -0,0 +1,12 @@
+package br.com.devlucasyuji.camerareminder
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class CameraReminderApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
--- shared/themes/src/main/java/br/com/devlucasyuji/themes/Type.kt
+++ shared/themes/src/main/java/br/com/devlucasyuji/themes/Type.kt
@@ -5,8 +5,7 @@ import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.sp
-import br.com.devlucasyuji.camerareminder.R
+import br.com.devlucasyuji.camerareminder.shared.themes.R
 
 val Typography = Typography(
     bodyLarge = TextStyle(
