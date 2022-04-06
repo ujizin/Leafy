ba6728ed0ec04c013315c02860322325ecd23088
devLucasYuji <lucasyujideveloper@gmail.com>
2022-04-06 01:37:02 -0300

    build: add ktlint to gradle

--- data/local/src/test/java/br/com/devlucasyuji/local/ExampleUnitTest.kt
+++ data/local/src/test/java/br/com/devlucasyuji/local/ExampleUnitTest.kt
@@ -1,8 +1,7 @@
 package br.com.devlucasyuji.local
 
+import junit.framework.TestCase.assertEquals
 import org.junit.Test
-
-import org.junit.Assert.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
--- data/repository/src/test/java/br/com/devlucasyuji/repository/ExampleUnitTest.kt
+++ data/repository/src/test/java/br/com/devlucasyuji/repository/ExampleUnitTest.kt
@@ -1,8 +1,7 @@
 package br.com.devlucasyuji.repository
 
+import junit.framework.Assert.assertEquals
 import org.junit.Test
-
-import org.junit.Assert.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
