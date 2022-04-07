5eae1a9ba40b97e4c60a0399871d179accf544ac
devLucasYuji <lucasyujideveloper@gmail.com>
2022-04-07 03:16:02 -0300

    fix: change icons size to 32x32

--- shared/components/src/main/java/br/com/devlucasyuji/components/atomic/atoms/Icon.kt
+++ shared/components/src/main/java/br/com/devlucasyuji/components/atomic/atoms/Icon.kt
@@ -2,10 +2,12 @@ package br.com.devlucasyuji.components.atomic.atoms
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import br.com.devlucasyuji.components.Icons
 import br.com.devlucasyuji.components.extensions.OnClick
 
@@ -16,7 +18,9 @@ internal fun Icon(
     onClick: OnClick
 ) {
     Image(
-        modifier = modifier.clickable(onClick = onClick),
+        modifier = modifier
+            .size(32.dp)
+            .clickable(onClick = onClick),
         painter = painterResource(icon.resId),
         contentDescription = stringResource(icon.descriptionRes),
     )
