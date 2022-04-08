c27b357e1c56a105fa38f72ac6955ca3a24586c3
devLucasYuji <lucasyujideveloper@gmail.com>
2022-04-08 03:30:00 -0300

    fix: remove import wildcard

--- shared/components/src/main/java/br/com/devlucasyuji/components/atomic/molecules/Toolbar.kt
+++ shared/components/src/main/java/br/com/devlucasyuji/components/atomic/molecules/Toolbar.kt
@@ -1,6 +1,10 @@
 package br.com.devlucasyuji.components.atomic.molecules
 
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
