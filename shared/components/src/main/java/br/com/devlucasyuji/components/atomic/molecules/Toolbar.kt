80a8d7c88aa22e52b33de6df0e53eaa7de13dbed
devLucasYuji <lucasyujideveloper@gmail.com>
2022-04-27 00:52:51 -0300

    fix: package from icon

--- shared/components/src/main/java/br/com/devlucasyuji/components/atomic/molecules/Toolbar.kt
+++ shared/components/src/main/java/br/com/devlucasyuji/components/atomic/molecules/Toolbar.kt
@@ -9,7 +9,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import br.com.devlucasyuji.components.Icons
+import br.com.devlucasyuji.components.props.Icons
 import br.com.devlucasyuji.components.atomic.atoms.ButtonIcon
 
 @Composable
