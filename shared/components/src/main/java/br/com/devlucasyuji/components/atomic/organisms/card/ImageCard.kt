3195369bceab029303a400f796a08f77280ce879
devLucasYuji <lucasyujideveloper@gmail.com>
2022-05-11 22:46:41 -0300

    feat: add image card with movable content of

--- shared/components/src/main/java/br/com/devlucasyuji/components/atomic/organisms/card/ImageCard.kt
+++ shared/components/src/main/java/br/com/devlucasyuji/components/atomic/organisms/card/ImageCard.kt
@@ -1,11 +1,14 @@
 package br.com.devlucasyuji.components.atomic.organisms.card
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.movableContentOf
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.unit.dp
 import br.com.devlucasyuji.components.animation.Animate.Animated
@@ -14,6 +17,8 @@ import br.com.devlucasyuji.components.atomic.atoms.ButtonIcon
 import br.com.devlucasyuji.components.atomic.molecules.BoxImage
 import br.com.devlucasyuji.components.atomic.molecules.TitleRow
 import br.com.devlucasyuji.components.props.Text
+import coil.compose.AsyncImagePainter
+import coil.compose.rememberAsyncImagePainter
 
 @Composable
 fun ImageCard(
@@ -27,22 +32,27 @@ fun ImageCard(
     animation: Animation = Animation.SlideToTop,
     vararg buttonIcons: ButtonIcon?,
 ) {
-    animation.Animated {
+    val painter = rememberAsyncImagePainter(data)
+    val content = movableContentOf {
         BoxImage(
             modifier = Modifier
                 .fillMaxWidth()
                 .height(size.height)
                 .then(modifier)
-                .clip(RoundedCornerShape(4.dp))
-            ,
-            data = data,
+                .clip(RoundedCornerShape(4.dp)),
+            painter = painter,
             boxModifier = cardModifier,
             contentDescription = contentDescription,
             contentAlignment = Alignment.BottomStart
         ) {
-            TitleRow(title = title, subTitle = subTitle) {
+            TitleRow(title = title, subTitle = subTitle, verticalAlignment = Alignment.Bottom) {
                 buttonIcons.forEach { it?.ButtonIcon() }
             }
         }
+    }
+
+    when (painter.state) {
+        is AsyncImagePainter.State.Success -> animation.Animated { content() }
+        else -> Box(Modifier.alpha(0F)) { content() }
     }
 }
