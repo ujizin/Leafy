2d6ee0eb73f8abe467a4340fad8a8fa692cd352c
devLucasYuji <lucasyujideveloper@gmail.com>
2022-06-29 22:47:32 -0300

    feat: provides photo's use cases by hilt

--- domain/src/main/java/br/com/devlucasyuji/domain/usecase/UseCaseModule.kt
+++ domain/src/main/java/br/com/devlucasyuji/domain/usecase/UseCaseModule.kt
@@ -1,8 +1,14 @@
 package br.com.devlucasyuji.domain.usecase
 
 import br.com.devlucasyuji.domain.repository.PhotoRepository
+import br.com.devlucasyuji.domain.usecase.photo.AddPhoto
+import br.com.devlucasyuji.domain.usecase.photo.DeletePhoto
+import br.com.devlucasyuji.domain.usecase.photo.implementation.AddPhotoImpl
 import br.com.devlucasyuji.domain.usecase.photo.LoadAllPhoto
-import br.com.devlucasyuji.domain.usecase.photo.LoadAllPhotoImpl
+import br.com.devlucasyuji.domain.usecase.photo.UpdatePhoto
+import br.com.devlucasyuji.domain.usecase.photo.implementation.DeletePhotoImpl
+import br.com.devlucasyuji.domain.usecase.photo.implementation.LoadAllPhotoImpl
+import br.com.devlucasyuji.domain.usecase.photo.implementation.UpdatePhotoImpl
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -18,4 +24,22 @@ object UseCaseModule {
     fun provideLoadAllPhotos(
         repository: PhotoRepository
     ): LoadAllPhoto = LoadAllPhotoImpl(repository)
+
+    @Provides
+    @Singleton
+    fun provideAddPhoto(
+        repository: PhotoRepository
+    ): AddPhoto = AddPhotoImpl(repository)
+
+    @Provides
+    @Singleton
+    fun provideUpdatePhoto(
+        repository: PhotoRepository
+    ): UpdatePhoto = UpdatePhotoImpl(repository)
+
+    @Provides
+    @Singleton
+    fun provideDeletePhoto(
+        repository: PhotoRepository
+    ): DeletePhoto = DeletePhotoImpl(repository)
 }
