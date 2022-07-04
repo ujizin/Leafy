0f0e211a1f68c2c99d704595f51e6e389d77620b
devLucasYuji <lucasyujideveloper@gmail.com>
2022-07-03 23:02:04 -0300

    feat: add photo ui state on home view model

--- features/home/src/main/java/br/com/devlucasyuji/home/HomeViewModel.kt
+++ features/home/src/main/java/br/com/devlucasyuji/home/HomeViewModel.kt
@@ -1,13 +1,37 @@
 package br.com.devlucasyuji.home
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import br.com.devlucasyuji.domain.model.Photo
 import br.com.devlucasyuji.domain.usecase.photo.LoadAllPhoto
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.stateIn
 
 @HiltViewModel
 class HomeViewModel @Inject constructor(
-    private val loadAllPhoto: LoadAllPhoto,
-): ViewModel() {
+    loadAllPhoto: LoadAllPhoto,
+) : ViewModel() {
 
+    private val photoState: StateFlow<UIState> = combine(loadAllPhoto()) { (photoResult) ->
+        if (photoResult.isSuccess) {
+            val photos = photoResult.getOrDefault(emptyList())
+            UIState.Success(photos)
+        } else {
+            UIState.Error(photoResult.exceptionOrNull())
+        }
+    }.stateIn(
+        viewModelScope,
+        SharingStarted.WhileSubscribed(5_000),
+        initialValue = UIState.Loading,
+    )
+}
+
+sealed interface UIState {
+    data class Success(val photos: List<Photo>) : UIState
+    data class Error(val throwable: Throwable?) : UIState
+    object Loading : UIState
 }
