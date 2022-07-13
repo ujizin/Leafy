3ebe366b3a9c6b92a1c4acbe0950c88d68ba86a6
devLucasYuji <lucasyujideveloper@gmail.com>
2022-07-12 22:11:06 -0300

    feat: add animated text composable

--- shared/components/src/main/java/br/com/devlucasyuji/components/ui/animated/AnimatedText.kt
+++ shared/components/src/main/java/br/com/devlucasyuji/components/ui/animated/AnimatedText.kt
@@ -1,12 +1,12 @@
-package br.com.devlucasyuji.components.atomic.atoms
+package br.com.devlucasyuji.components.ui.animated
 
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
-import br.com.devlucasyuji.components.animation.Animate.Animated
-import br.com.devlucasyuji.components.animation.Animation
+import br.com.devlucasyuji.components.ui.animated.animation.Animate.Animated
+import br.com.devlucasyuji.components.ui.animated.animation.Animation
 import br.com.devlucasyuji.themes.CameraReminderTheme
 
 @Composable
