9edce05b9eedef2a0b67aa136cfca1bb6a6fe81c
devLucasYuji <lucasyujideveloper@gmail.com>
2022-07-27 21:35:56 -0300

    feat: add new components to home and alarm section

--- features/alarm/src/main/java/br/com/devlucasyuji/alarm/AlarmSection.kt
+++ features/alarm/src/main/java/br/com/devlucasyuji/alarm/AlarmSection.kt
@@ -1,14 +1,15 @@
 package br.com.devlucasyuji.alarm
 
-import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.navigation.NavController
-import br.com.devlucasyuji.components.atomic.organisms.header
-import br.com.devlucasyuji.components.props.Text
+import br.com.devlucasyuji.components.Section
+import br.com.devlucasyuji.components.extensions.section
 
 @Composable
 fun NavController.AlarmSection() {
-    LazyColumn {
-        header(title = Text("Alarm"))
-    }
+    Section(
+        modifier = Modifier.section(),
+        title = "Alarm"
+    )
 }
