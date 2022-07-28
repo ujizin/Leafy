dafff1830c32bc2c10cb2cf2653bc441aba89598
devLucasYuji <lucasyujideveloper@gmail.com>
2022-07-27 21:35:56 -0300

    feat: add new components to home and alarm section

--- features/home/src/main/java/br/com/devlucasyuji/home/HomeSection.kt
+++ features/home/src/main/java/br/com/devlucasyuji/home/HomeSection.kt
@@ -1,31 +1,35 @@
 package br.com.devlucasyuji.home
 
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyItemScope
+import androidx.compose.foundation.lazy.LazyListScope
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
-import br.com.devlucasyuji.components.animation.Animation
-import br.com.devlucasyuji.components.atomic.atoms.ButtonIcon
-import br.com.devlucasyuji.components.atomic.organisms.Header
-import br.com.devlucasyuji.components.atomic.organisms.Section
-import br.com.devlucasyuji.components.atomic.organisms.card.CardSize
-import br.com.devlucasyuji.components.atomic.organisms.card.ImageCard
+import br.com.devlucasyuji.components.ui.animated.animation.Animation
+import br.com.devlucasyuji.components.ui.animated.AnimatedIcon
+import br.com.devlucasyuji.components.ui.card.CardSize
+import br.com.devlucasyuji.components.ui.card.BoxImage
 import br.com.devlucasyuji.components.extensions.capitalize
-import br.com.devlucasyuji.components.extensions.innerShadow
-import br.com.devlucasyuji.components.props.Icons
-import br.com.devlucasyuji.components.props.Shadow
-import br.com.devlucasyuji.components.props.Text
+import br.com.devlucasyuji.components.ui.image.Icons
+import br.com.devlucasyuji.components.Section
+import br.com.devlucasyuji.components.extensions.section
+import br.com.devlucasyuji.components.ui.label.TitleRow
 import br.com.devlucasyuji.domain.model.Photo
 import java.io.File
 
@@ -33,31 +37,56 @@ import java.io.File
 fun NavController.HomeRoute(viewModel: HomeViewModel = hiltViewModel()) {
     val state by viewModel.homeState.collectAsState()
     when (val result: UIState = state) {
-        UIState.Loading -> TODO()
+        UIState.Loading -> {}
         is UIState.Success -> HomeScreen(result)
-        is UIState.Error -> TODO()
+        is UIState.Error -> {}
     }
 }
 
 @Composable
 private fun HomeScreen(result: UIState.Success) {
-    Section(
-        header = Header(
-            title = Text(
-                stringResource(
+    NavLazyColumn {
+        item {
+            Section(
+                modifier = Modifier.section(),
+                title = stringResource(
                     id = R.string.hello_user,
                     result.nickname.capitalize()
-                ).capitalize()
-            ),
-            subTitle = Text(stringResource(id = R.string.welcome_back).capitalize()),
-            leadingIcon = ButtonIcon(Icons.Hamburger, Animation.SlideToEnd),
-            trailingIcon = ButtonIcon(Icons.Magnifier, Animation.SlideToStart)
-        ),
-    ) {
-        when {
-            result.photos.isEmpty() -> item { EmptyPhotoCard() }
-            else -> items(result.photos, key = { it.id }) { HomePhotoCard(it) }
+                ).capitalize(),
+                subTitle = stringResource(id = R.string.welcome_back).capitalize(),
+                leadingIcon = {
+                    AnimatedIcon(icon = Icons.Hamburger, animation = Animation.SlideToEnd)
+                },
+                trailingIcon = {
+                    AnimatedIcon(icon = Icons.Magnifier, animation = Animation.SlideToStart)
+                }
+            )
         }
+        when {
+//                result.photos.isEmpty() -> item { EmptyPhotoCard() }
+            else -> items(20) {
+                HomePhotoCard(
+                    Photo(
+                        0,
+                        "yuji apenas",
+                        "",
+                        "",
+                        "eu apenas",
+                        false,
+                        1
+                    )
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun NavLazyColumn(modifier: Modifier = Modifier, content: LazyListScope.() -> Unit) {
+    LazyColumn {
+        item { Spacer(modifier = modifier) }
+        content()
+        item { Spacer(Modifier.size(32.dp)) }
     }
 }
 
@@ -69,20 +98,22 @@ private fun EmptyPhotoCard() {
 @Composable
 @OptIn(ExperimentalFoundationApi::class)
 private fun LazyItemScope.HomePhotoCard(photo: Photo) {
-    ImageCard(
+    BoxImage(
         modifier = Modifier
             .animateItemPlacement()
+            .height(CardSize.Large.height)
             .padding(horizontal = 20.dp, vertical = 8.dp),
-        cardModifier = Modifier
-            .innerShadow(Shadow.Black, RoundedCornerShape(4.dp))
-            .padding(12.dp),
-        data = File(photo.filePath),
+        data = "https://pps.whatsapp.net/v/t61.24694-24/179134053_520513506372508_3465206539243170804_n.jpg?ccb=11-4&oh=01_AVxbQe01QoC8ku5i0d0k1Kd76j0_dhk89ISzNOIoEL7bSw&oe=62F2ADD0",
         contentDescription = photo.description,
-        title = Text(photo.title, TextStyle(color = Color.White)),
-        subTitle = Text(photo.description, TextStyle(color = Color.White)),
-        size = CardSize.Large,
-        buttonIcons = arrayOf(
-            ButtonIcon(Icons.Shared, tint = Color.White, size = 24.dp),
-        )
-    )
+    ) {
+        TitleRow(
+            title = photo.title,
+            titleStyle = MaterialTheme.typography.titleLarge.copy(color = Color.White),
+            subTitleStyle = MaterialTheme.typography.bodyMedium.copy(color = Color.White),
+            subTitle = photo.description,
+            verticalAlignment = Alignment.Bottom
+        ) {
+            AnimatedIcon(icon = Icons.Shared, tint = Color.White, size = 24.dp)
+        }
+    }
 }
