0338ce528909aadc48dc483387baca53eacc4889
devLucasYuji <lucasyujideveloper@gmail.com>
2022-08-07 20:28:15 -0300

    feat: add extenstions to result

--- domain/src/main/java/br/com/devlucasyuji/domain/result/ResultExtensions.kt
+++ domain/src/main/java/br/com/devlucasyuji/domain/result/ResultExtensions.kt
@@ -1,15 +1,9 @@
-package br.com.devlucasyuji.domain.usecase
+package br.com.devlucasyuji.domain.result
 
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.onStart
-
-sealed interface Result<out T> {
-    data class Success<T>(val data: T) : Result<T>
-    data class Error(val exception: Throwable? = null) : Result<Nothing>
-    object Loading : Result<Nothing>
-}
 
 fun <T> Flow<T>.asResult(): Flow<Result<T>> = map<T, Result<T>> {
     Result.Success(it)
@@ -17,4 +11,34 @@ fun <T> Flow<T>.asResult(): Flow<Result<T>> = map<T, Result<T>> {
     emit(Result.Loading)
 }.catch {
     emit(Result.Error(it))
+}
+
+/**
+ * Check if there are any errors on results.
+ *
+ * @param result list of results
+ * @param block callback that'll be called if there's an error
+ * */
+inline fun <T : Any> ifAnyError(
+    vararg result: Result<T>, block: (List<Throwable>) -> Unit
+) {
+    val errors = result.filterIsInstance<Result.Error>().mapNotNull { it.exception }
+    if (errors.isNotEmpty()) block(errors)
+}
+
+/**
+ * Check if results is successful and transform data on callback.
+ *
+ *  @param result first result
+ *  @param result2 second result to be combined.
+ *  @param transform callback with transformed data
+ * */
+inline fun <R1, R2> ifSuccess(
+    result: Result<R1>,
+    result2: Result<R2>,
+    transform: (R1, R2) -> Unit
+) {
+    if (result is Result.Success && result2 is Result.Success) {
+        transform(result.data, result2.data)
+    }
 }
