9911ecea2d45b89941b08f257be4ace9952d897e
devLucasYuji <lucasyujideveloper@gmail.com>
2022-08-09 20:30:08 -0300

    feat: handle with combined state on viewmodel

--- features/home/src/main/java/br/com/devlucasyuji/home/HomeViewModel.kt
+++ features/home/src/main/java/br/com/devlucasyuji/home/HomeViewModel.kt
@@ -3,7 +3,10 @@ package br.com.devlucasyuji.home
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import br.com.devlucasyuji.domain.model.Photo
+import br.com.devlucasyuji.domain.result.ifAnyError
+import br.com.devlucasyuji.domain.result.ifSuccess
 import br.com.devlucasyuji.domain.usecase.photo.LoadAllPhoto
+import br.com.devlucasyuji.domain.usecase.user.LoadUser
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 import kotlinx.coroutines.flow.SharingStarted
@@ -14,32 +17,41 @@ import kotlinx.coroutines.flow.stateIn
 @HiltViewModel
 class HomeViewModel @Inject constructor(
     loadAllPhoto: LoadAllPhoto,
+    loadUser: LoadUser,
 ) : ViewModel() {
 
-    val homeState: StateFlow<UIState> = combine(loadAllPhoto()) { (photoResult) ->
-        if (photoResult.isSuccess) {
-            val photos = photoResult.getOrDefault(emptyList())
-            // TODO get name from user data store
-            UIState.Success("user", photos)
-        } else {
-            UIState.Error(photoResult.exceptionOrNull())
-        }
-    }.stateIn(
-        viewModelScope,
-        SharingStarted.WhileSubscribed(5_000),
-        initialValue = UIState.Loading,
-    )
+    val homeState: StateFlow<HomeUIState> =
+        combine(
+            loadAllPhoto(),
+            loadUser(),
+        ) { photoResult, userResult ->
+
+            ifAnyError(userResult, photoResult) {
+                return@combine HomeUIState.Error(it.first())
+            }
+
+            ifSuccess(userResult, photoResult) { user, photos ->
+                return@combine HomeUIState.Success(user.nickname, photos)
+            }
+
+            return@combine HomeUIState.Loading
+        }.stateIn(
+            viewModelScope,
+            SharingStarted.WhileSubscribed(5_000),
+            initialValue = HomeUIState.Loading,
+        )
 
     fun takePicture() {
         // TODO navigate to picture section
     }
 }
 
-sealed interface UIState {
+sealed interface HomeUIState {
     data class Success(
         val nickname: String,
         val photos: List<Photo>
-    ) : UIState
-    data class Error(val throwable: Throwable?) : UIState
-    object Loading : UIState
+    ) : HomeUIState
+
+    data class Error(val throwable: Throwable?) : HomeUIState
+    object Loading : HomeUIState
 }
