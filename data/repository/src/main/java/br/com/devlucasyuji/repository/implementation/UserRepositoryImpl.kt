f538a7c39917d1cb58788a98030beda2a67b930b
devLucasYuji <lucasyujideveloper@gmail.com>
2022-08-18 20:57:32 -0300

    feat: add constructor to user repository implementation

--- data/repository/src/main/java/br/com/devlucasyuji/repository/implementation/UserRepositoryImpl.kt
+++ data/repository/src/main/java/br/com/devlucasyuji/repository/implementation/UserRepositoryImpl.kt
@@ -2,9 +2,17 @@ package br.com.devlucasyuji.repository.implementation
 
 import br.com.devlucasyuji.domain.model.User
 import br.com.devlucasyuji.domain.repository.UserRepository
+import br.com.devlucasyuji.repository.datasource.UserDataSource
+import br.com.devlucasyuji.repository.mapper.UserMapper
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
 
-class UserRepositoryImpl: UserRepository {
+internal class UserRepositoryImpl(
+    private val datasource: UserDataSource,
+    private val userMapper: UserMapper,
+    private val dispatcher: CoroutineDispatcher = Dispatchers.IO
+) : UserRepository {
 
     override fun getUser(): Flow<User> {
         TODO("Not yet implemented")
