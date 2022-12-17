87e77df1ce56b30b5a858d8dcbbfbc7f0e592b36
ujizin <lucasyujideveloper@gmail.com>
2022-12-16 21:31:51 -0300

    feat: separate icons strings

--- shared/components/src/main/java/br/com/devlucasyuji/components/ui/image/Icons.kt
+++ shared/components/src/main/java/br/com/devlucasyuji/components/ui/image/Icons.kt
@@ -18,5 +18,6 @@ enum class Icons(
     Add(R.drawable.add, R.string.add),
     Shared(R.drawable.shared, R.string.shared),
     Folder(R.drawable.folder, R.string.folder),
-    Others(R.drawable.others, R.string.others);
+    Others(R.drawable.others, R.string.others),
+    Close(R.drawable.close, R.string.close);
 }
