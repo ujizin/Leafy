418bbc5aeabe45437112ffa761a51d49aafa0c3c
ujizin <lucasyujideveloper@gmail.com>
2023-01-28 23:55:48 -0300

    feat: add placeholder for text field

--- shared/components/src/main/java/br/com/devlucasyuji/components/ui/textfield/TextField.kt
+++ shared/components/src/main/java/br/com/devlucasyuji/components/ui/textfield/TextField.kt
@@ -8,16 +8,21 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material3.LocalTextStyle
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.graphics.SolidColor
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import br.com.devlucasyuji.components.extensions.capitalize
+import br.com.devlucasyuji.components.ui.animated.animation.Animate.Animated
+import br.com.devlucasyuji.components.ui.animated.animation.Animation
 import br.com.devlucasyuji.themes.CameraReminderTheme
 
 @Composable
@@ -27,23 +32,32 @@ fun TextField(
     singleLine: Boolean = true,
     shape: Shape = RoundedCornerShape(4.dp),
     paddingValues: PaddingValues = PaddingValues(16.dp),
+    animation: Animation = Animation.SlideToTop,
+    placeholder: String? = null,
     onValueChange: (String) -> Unit,
 ) {
-    BasicTextField(
-        modifier = modifier,
-        value = value,
-        onValueChange = onValueChange,
-        singleLine = singleLine,
-        textStyle = LocalTextStyle.current,
-        cursorBrush = SolidColor(MaterialTheme.colorScheme.primary),
-        decorationBox = { innerTextField ->
-            Box(
-                Modifier
-                    .background(MaterialTheme.colorScheme.surfaceVariant, shape)
-                    .padding(paddingValues)
-            ) { innerTextField() }
-        }
-    )
+    Animated(animation = animation) {
+        BasicTextField(
+            modifier = modifier,
+            value = value,
+            onValueChange = onValueChange,
+            singleLine = singleLine,
+            textStyle = LocalTextStyle.current,
+            cursorBrush = SolidColor(MaterialTheme.colorScheme.primary),
+            decorationBox = { innerTextField ->
+                Box(
+                    Modifier
+                        .background(MaterialTheme.colorScheme.surfaceVariant, shape)
+                        .padding(paddingValues)
+                ) {
+                    innerTextField()
+                    if (placeholder != null && value.isEmpty()) {
+                        Text(text = placeholder.capitalize(), color = Color(0x7F3F4549))
+                    }
+                }
+            }
+        )
+    }
 }
 
 @Preview
