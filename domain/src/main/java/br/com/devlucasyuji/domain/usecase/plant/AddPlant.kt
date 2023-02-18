85fb44ab8fff7ff26e110441ffec30fd4215ad60
ujizin <lucasyujideveloper@gmail.com>
2023-02-18 12:38:04 -0300

    refactor: change interface from add photo to add plant

---
+++ domain/src/main/java/br/com/devlucasyuji/domain/usecase/plant/AddPlant.kt
@@ -0,0 +1,17 @@
+package br.com.devlucasyuji.domain.usecase.plant
+
+import br.com.devlucasyuji.domain.model.Plant
+import kotlinx.coroutines.flow.Flow
+
+/**
+ * Use case to insert plant in the data source.
+ * */
+interface AddPlant {
+
+    /**
+     * Add a plant.
+     *
+     * @param plants plants to be added
+     * */
+    operator fun invoke(vararg plants: Plant): Flow<Unit>
+}
