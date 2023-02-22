ccad2a38d610309f432a8ca7746c60f1a2638661
ujizin <lucasyujideveloper@gmail.com>
2023-02-22 20:28:47 -0300

    fix: remove movable content of due to crash

--- shared/components/src/main/java/br/com/devlucasyuji/components/ui/card/ImageCard.kt
+++ shared/components/src/main/java/br/com/devlucasyuji/components/ui/card/ImageCard.kt
@@ -2,14 +2,13 @@ package br.com.devlucasyuji.components.ui.card
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.movableContentOf
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
+import br.com.devlucasyuji.components.extensions.Content
 import br.com.devlucasyuji.components.ui.animated.animation.Animate.Animated
 import br.com.devlucasyuji.components.ui.animated.animation.Animation
 import br.com.devlucasyuji.components.ui.image.BoxImage
-import br.com.devlucasyuji.components.extensions.Content
 import coil.compose.AsyncImagePainter
 import coil.compose.rememberAsyncImagePainter
 
@@ -22,17 +21,24 @@ fun BoxImage(
     innerContent: @Composable Content = {}
 ) {
     val painter = rememberAsyncImagePainter(data)
-    val content = movableContentOf {
-        BoxImage(
-            modifier = modifier,
-            painter = painter,
-            contentDescription = contentDescription,
-            contentAlignment = Alignment.BottomStart
-        ) { innerContent() }
-    }
 
     when (painter.state) {
-        is AsyncImagePainter.State.Success -> Animated(animation = animation) { content() }
-        else -> Box(Modifier.alpha(0F)) { content() }
+        is AsyncImagePainter.State.Success -> Animated(animation = animation) {
+            BoxImage(
+                modifier = modifier,
+                painter = painter,
+                contentDescription = contentDescription,
+                contentAlignment = Alignment.BottomStart
+            ) { innerContent() }
+        }
+
+        else -> Box(Modifier.alpha(0F)) {
+            BoxImage(
+                modifier = modifier,
+                painter = painter,
+                contentDescription = contentDescription,
+                contentAlignment = Alignment.BottomStart
+            ) { innerContent() }
+        }
     }
 }
