fe4aeccbb6254e65c5cb930baf1f86a04b2e30da
ujizin <lucasyujideveloper@gmail.com>
2023-03-05 23:56:59 -0300

    feat: hide bottom navigation bar when there is no BottomNavItem destination

--- shared/components/src/main/java/br/com/devlucasyuji/components/ui/navigation/bottombar/NavigationBar.kt
+++ shared/components/src/main/java/br/com/devlucasyuji/components/ui/navigation/bottombar/NavigationBar.kt
@@ -3,6 +3,7 @@ package br.com.devlucasyuji.components.ui.navigation.bottombar
 import androidx.compose.foundation.layout.offset
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
@@ -10,11 +11,20 @@ import br.com.devlucasyuji.components.ui.animated.animation.Animate.Animated
 import br.com.devlucasyuji.components.ui.animated.animation.Animation
 import br.com.devlucasyuji.components.ui.button.CameraButton
 import br.com.devlucasyuji.components.ui.navigation.currentNavItemAsState
+import br.com.devlucasyuji.navigation.Destination
 import br.com.devlucasyuji.navigation.navigate
 
 @Composable
 internal fun NavigationBar(navController: NavController) {
-    val navItem by navController.currentNavItemAsState(BottomNavItem.Home)
+    val navItem by navController.currentNavItemAsState<BottomNavItem>()
+    val hideBottomNavigationBar = remember(navItem) {
+        BottomNavItem.values().none { destination ->
+            destination.destination != Destination.Camera && destination.destination == navItem?.destination
+        }
+    }
+
+    if (hideBottomNavigationBar) return
+
     Animated(animation = Animation.SlideToTop.copy(delayMillis = Animation.SmallDelay)) {
         androidx.compose.material3.NavigationBar {
             BottomNavItem.values().forEach { item ->
