35478adab71dd9a2db0956563cd62fdb94c6b1f1
ujizin <lucasyujideveloper@gmail.com>
2023-03-11 09:52:19 -0300

    feat: load image with original size

--- shared/components/src/main/java/br/com/devlucasyuji/components/ui/card/ImageCard.kt
+++ shared/components/src/main/java/br/com/devlucasyuji/components/ui/card/ImageCard.kt
@@ -1,7 +1,9 @@
 package br.com.devlucasyuji.components.ui.card
 
-import androidx.compose.foundation.layout.Box
+import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
@@ -23,31 +25,28 @@ fun BoxImage(
     animation: Animation = Animation.SlideToTop,
     innerContent: @Composable Content = {}
 ) {
-    val model = ImageRequest.Builder(LocalContext.current)
-        .data(data)
-        .size(Size.ORIGINAL)
-        .crossfade(true)
-        .build()
-
+    val context = LocalContext.current
+    val model = remember(data) {
+        ImageRequest.Builder(context)
+            .data(data)
+            .size(Size.ORIGINAL)
+            .crossfade(true)
+            .build()
+    }
     val painter = rememberAsyncImagePainter(model = model)
 
-    when (painter.state) {
-        is AsyncImagePainter.State.Success -> Animated(animation = animation) {
-            BoxImage(
-                modifier = modifier,
-                painter = painter,
-                contentDescription = contentDescription,
-                contentAlignment = Alignment.BottomStart
-            ) { innerContent() }
-        }
-
-        else -> Box(Modifier.alpha(0F)) {
-            BoxImage(
-                modifier = modifier,
-                painter = painter,
-                contentDescription = contentDescription,
-                contentAlignment = Alignment.BottomStart
-            ) { innerContent() }
-        }
+    val alphaAnimated by animateFloatAsState(
+        targetValue = if (painter.state is AsyncImagePainter.State.Success) 1F else 0F
+    )
+    Animated(
+        modifier = Modifier.alpha(alphaAnimated),
+        animation = animation
+    ) {
+        BoxImage(
+            modifier = modifier,
+            painter = painter,
+            contentDescription = contentDescription,
+            contentAlignment = Alignment.BottomStart
+        ) { innerContent() }
     }
 }
