4da47896a7c5a34c6728b05fad79ca4095464630
ujizin <lucas.yuji@enjoei.com.br>
2023-03-16 18:22:19 -0300

    feat: add empty section for search empty list

--- features/search/src/main/java/br/com/devlucasyuji/search/ui/SearchEmptyList.kt
+++ features/search/src/main/java/br/com/devlucasyuji/search/ui/SearchEmptyList.kt
@@ -2,9 +2,24 @@ package br.com.devlucasyuji.search.ui
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import br.com.devlucasyuji.components.extensions.OnClick
+import br.com.devlucasyuji.components.extensions.capitalize
+import br.com.devlucasyuji.components.ui.EmptySection
+import br.com.devlucasyuji.search.R
 
 @Composable
-internal fun SearchEmptyList(modifier: Modifier = Modifier) {
-
-
+internal fun SearchEmptyList(
+    searchText: String,
+    modifier: Modifier = Modifier,
+    onTakePictureClick: OnClick,
+) {
+    EmptySection(
+        modifier = modifier,
+        description = stringResource(
+            id = R.string.search_empty_description,
+            searchText
+        ).capitalize(),
+        onClick = onTakePictureClick,
+    )
 }
