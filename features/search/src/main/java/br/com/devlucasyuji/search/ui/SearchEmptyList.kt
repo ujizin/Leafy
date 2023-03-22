62b4c3d0eb4d386e4f581bf9da14b1049b3e7731
ujizin <lucasyujideveloper@gmail.com>
2023-03-22 04:19:07 -0300

    feat: add default empty text if there's no plant on search

--- features/search/src/main/java/br/com/devlucasyuji/search/ui/SearchEmptyList.kt
+++ features/search/src/main/java/br/com/devlucasyuji/search/ui/SearchEmptyList.kt
@@ -1,12 +1,14 @@
 package br.com.devlucasyuji.search.ui
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.platform.LocalContext
 import br.com.devlucasyuji.components.extensions.OnClick
 import br.com.devlucasyuji.components.extensions.capitalize
 import br.com.devlucasyuji.components.ui.EmptySection
 import br.com.devlucasyuji.search.R
+import br.com.devlucasyuji.components.R as CR
 
 @Composable
 internal fun SearchEmptyList(
@@ -14,12 +16,18 @@ internal fun SearchEmptyList(
     modifier: Modifier = Modifier,
     onTakePictureClick: OnClick,
 ) {
+    val context = LocalContext.current
+    val description = remember(searchText) {
+        if (searchText.isEmpty()) {
+            context.getString(CR.string.empty_plant)
+        } else {
+            context.getString(R.string.search_empty_description, searchText)
+        }
+    }.capitalize()
+
     EmptySection(
         modifier = modifier,
-        description = stringResource(
-            id = R.string.search_empty_description,
-            searchText
-        ).capitalize(),
+        description = description,
         onClick = onTakePictureClick,
     )
 }
