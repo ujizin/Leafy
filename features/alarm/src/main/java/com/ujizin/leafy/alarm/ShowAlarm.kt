b12ac78fee6fa207a3e11b9d1af1f36ab181b249
ujizin <lucas.yuji@enjoei.com.br>
2023-04-12 16:37:11 -0300

    feat: disable the alarm ringtone when it is not in use

--- features/alarm/src/main/java/com/ujizin/leafy/alarm/ShowAlarm.kt
+++ features/alarm/src/main/java/com/ujizin/leafy/alarm/ShowAlarm.kt
@@ -5,6 +5,9 @@ import android.content.Intent
 import android.os.Build
 import com.ujizin.leafy.alarm.receiver.AlarmReceiver
 import com.ujizin.leafy.core.components.R
+import com.ujizin.leafy.domain.model.Alarm
+import com.ujizin.leafy.domain.model.Plant
+import com.ujizin.leafy.domain.model.WeekDay
 import com.ujizin.leafy.domain.result.mapResult
 import com.ujizin.leafy.domain.usecase.alarm.LoadAlarm
 import com.ujizin.leafy.domain.usecase.plant.LoadPlant
@@ -12,6 +15,7 @@ import kotlinx.coroutines.FlowPreview
 import kotlinx.coroutines.flow.emptyFlow
 import kotlinx.coroutines.flow.flatMapConcat
 import kotlinx.coroutines.flow.map
+import java.util.Calendar
 
 /**
  * Show alarm use case
@@ -28,19 +32,32 @@ class ShowAlarm(
     ) = loadAlarm(intent.getLongExtra(AlarmReceiver.ALARM_ID_EXTRA, -1))
         .mapResult()
         .flatMapConcat { alarm ->
-            if (!alarm.enabled) return@flatMapConcat emptyFlow()
+            if (!alarm.enabled && !alarm.checkDayOfTheWeek()) return@flatMapConcat emptyFlow()
 
             loadPlant(alarm.plantId).mapResult().map { plant ->
                 val ringtone = intent.getStringExtra(AlarmReceiver.RINGTONE_CONTENT_EXTRA)
-                val serviceIntent = Intent(context, AlarmService::class.java).apply {
-                    putExtra(AlarmService.TITLE_ARG, context.getString(R.string.app_name))
-                    putExtra(AlarmService.DESCRIPTION_ARG, plant.title)
-                    putExtra(AlarmService.RINGTONE_URI_STRINGIFY_ARG, ringtone)
-                }
+                val serviceIntent = getAlarmServiceIntent(context, plant, ringtone)
 
                 startAlarmService(context, serviceIntent)
             }
         }
+
+    private fun Alarm.checkDayOfTheWeek(): Boolean {
+        val calendar = Calendar.getInstance()
+        return weekDays.contains(
+            WeekDay.values()[calendar.get(Calendar.DAY_OF_WEEK)]
+        )
+    }
+
+    private fun getAlarmServiceIntent(
+        context: Context,
+        plant: Plant,
+        ringtone: String?,
+    ) = Intent(context, AlarmService::class.java).apply {
+        putExtra(AlarmService.TITLE_ARG, context.getString(R.string.app_name))
+        putExtra(AlarmService.DESCRIPTION_ARG, plant.title)
+        putExtra(AlarmService.RINGTONE_URI_STRINGIFY_ARG, ringtone)
+    }
 
     private fun startAlarmService(context: Context, serviceIntent: Intent) {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
