36da61ad1b9e7e33e9d7cda0fb61bed098f9b73f
ujizin <lucas.yuji@enjoei.com.br>
2023-08-28 21:47:17 -0300

    feat: add i18n to themes selector

---
+++ features/preferences/src/main/java/com/ujizin/leafy/preferences/extensions/ThemeExtensions.kt
@@ -0,0 +1,13 @@
+package com.ujizin.leafy.preferences.extensions
+
+import androidx.annotation.StringRes
+import com.ujizin.leafy.domain.model.Theme
+import com.ujizin.leafy.features.preferences.R
+
+val Theme.displayResId: Int
+    @StringRes
+    get() = when (this) {
+        Theme.System -> R.string.system
+        Theme.Dark -> R.string.dark
+        Theme.Light -> R.string.light
+    }
--- features/preferences/src/main/java/com/ujizin/leafy/preferences/ui/ThemeSelector.kt
+++ features/preferences/src/main/java/com/ujizin/leafy/preferences/ui/ThemeSelector.kt
@@ -6,6 +6,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import com.ujizin.leafy.core.ui.components.selector.ModalSelector
 import com.ujizin.leafy.core.ui.components.selector.ModalValue
@@ -13,6 +14,7 @@ import com.ujizin.leafy.core.ui.components.selector.Selector
 import com.ujizin.leafy.core.ui.extensions.capitalize
 import com.ujizin.leafy.domain.model.Theme
 import com.ujizin.leafy.features.preferences.R
+import com.ujizin.leafy.preferences.extensions.displayResId
 
 @Composable
 fun ThemeSelector(
@@ -20,13 +22,19 @@ fun ThemeSelector(
     theme: Theme,
     onThemeChanged: (Theme) -> Unit,
 ) {
+    val context = LocalContext.current
     var showModal by remember { mutableStateOf(false) }
-    val themes = remember { Theme.values().map { ModalValue(it.name, it) } }
-    val currentTheme = remember(theme) { ModalValue(theme.name, theme) }
+    val themes = remember {
+        Theme.values().map { ModalValue(context.getString(it.displayResId), it) }
+    }
+    val currentTheme = remember(theme) {
+        ModalValue(context.getString(theme.displayResId), theme)
+    }
+
     Selector(
         modifier = modifier,
         title = stringResource(R.string.app_theme).capitalize(),
-        subTitle = theme.toString(),
+        subTitle = stringResource(theme.displayResId),
         showModal = showModal,
         onModalStateChanged = { showModal = it },
     ) {
