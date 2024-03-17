7a43afb8de4d60c7adf2a485db474a169a62a3a7
ujizin <lucasyujideveloper@gmail.com>
2024-03-17 13:53:50 +0000

    fix: add i18n on share text

--- core/ui/src/main/java/com/ujizin/leafy/core/ui/extensions/PlantExtensions.kt
+++ core/ui/src/main/java/com/ujizin/leafy/core/ui/extensions/PlantExtensions.kt
@@ -3,6 +3,7 @@ package com.ujizin.leafy.core.ui.extensions
 import android.content.Context
 import android.content.Intent
 import androidx.core.content.FileProvider
+import com.ujizin.leafy.core.components.R
 import com.ujizin.leafy.domain.model.Plant
 
 fun Plant.share(context: Context) {
@@ -17,5 +18,10 @@ fun Plant.share(context: Context) {
         putExtra(Intent.EXTRA_STREAM, uri)
         putExtra(Intent.EXTRA_TEXT, "$title - $description")
     }
-    context.startActivity(Intent.createChooser(intent, "Share plant using"))
+    context.startActivity(
+        Intent.createChooser(
+            intent,
+            context.getString(R.string.plant_share_title).capitalize()
+        )
+    )
 }
