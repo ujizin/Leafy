f0b2d5f1f149a7567d769389bf6d43c0979945c7
ujizin <lucasyujideveloper@gmail.com>
2024-04-08 11:52:13 +0100

    chore(publish): apply spotless

--- features/camera/src/main/java/com/ujizin/leafy/camera/ui/CameraDenied.kt
+++ features/camera/src/main/java/com/ujizin/leafy/camera/ui/CameraDenied.kt
@@ -1,7 +1,6 @@
 package com.ujizin.leafy.camera.ui
 
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
