86f4e3218c71ea39e085306b5934c3b6d2f82c11
ujizin <lucasyujideveloper@gmail.com>
2024-05-12 21:46:59 +0100

    build: migrate features build gradle to kts

---
+++ features/alarm/build.gradle.kts
@@ -0,0 +1,26 @@
+plugins {
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlin.android)
+}
+
+apply(from = "$rootDir/config-compose.gradle")
+apply(from = "$rootDir/config-android.gradle")
+
+android {
+    namespace = "com.ujizin.leafy.features.alarm"
+
+    testOptions {
+        unitTests.isReturnDefaultValues = true
+    }
+}
+
+dependencies {
+    implementation(projects.core.navigation)
+    implementation(projects.core.ui)
+    implementation(projects.core.themes)
+    implementation(projects.domain)
+
+    testImplementation(projects.core.test)
+    testImplementation(libs.bundles.test)
+    testImplementation(libs.coroutines.test)
+}
