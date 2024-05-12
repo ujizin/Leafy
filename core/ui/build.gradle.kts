8ca230c35c5646f2cf1a8df659f6d3f909418d29
ujizin <lucasyujideveloper@gmail.com>
2024-05-12 21:35:19 +0100

    build: migrate core:ui build gradle to kts

---
+++ core/ui/build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlin.android)
+}
+
+apply(from = "$rootDir/config-compose.gradle")
+apply(from = "$rootDir/config-android.gradle")
+
+android {
+    namespace = "com.ujizin.leafy.core.components"
+}
+
+dependencies {
+    implementation(projects.core.themes)
+    implementation(projects.core.navigation)
+    implementation(projects.domain)
+
+    implementation(libs.coil)
+    implementation(libs.accompanist.permissions)
+    implementation(libs.androidx.app.compat)
+}
